130498efafeefaba0a1cd142d8c94982e197e0f0
m-klimaszewski <56638995+m-klimaszewski@users.noreply.github.com>
2021-07-14 22:23:44 +0200

    detectshippnglabel
    add algorithm  detection of shpipping label

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner_Lib scanner_lib = new Scanner_Lib();
-        //scanner_lib.displayImg("src/main/resources/img.jpg");
-        scanner_lib.grayScaleImg("src/main/resources/sl1.jpg");
-        System.out.println(Core.VERSION);
+
+        for (int x = 34; x<=40; x++){
+            String numOfImg = String.valueOf(x);
+            String path = "src/main/resources/sl ("+numOfImg+").jpg";
+            scanner_lib.runApp(path);
+            System.out.println(x);
+        }
+
+
+       // scanner_lib.runApp("src/main/resources/sl (33).jpg");
+
+         System.out.println(Core.VERSION);
     }
 }
--- src/main/java/Scanner_Lib.java
+++ src/main/java/Scanner_Lib.java
@@ -1,53 +1,177 @@
-import jdk.jfr.Threshold;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
+import org.opencv.core.*;
+import org.opencv.core.Point;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.highgui.*;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.core.Core;
-import org.opencv.objdetect.Objdetect;
+import org.opencv.imgproc.Moments;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.NoninvertibleTransformException;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
-public class Scanner_Lib  {
+public class Scanner_Lib {
 
-    public void displayImg(String path){
-        Mat img = Imgcodecs.imread(path);
-        HighGui.namedWindow("image", HighGui.WINDOW_AUTOSIZE);
-        HighGui.imshow("image", img);
-        HighGui.waitKey();
+    public void runApp(String path) throws IOException {
+      Mat inputImg = Imgcodecs.imread(path);
+      showImg(path, inputImg);
+      Mat outputImg = waybillDetector(inputImg);
+      showImg("outputImg",outputImg);
 
     }
-    public void grayScaleImg(String path) throws IOException {
-        Mat src = Imgcodecs.imread(path);
+
+
+    public Mat waybillDetector(Mat img) throws IOException {
+
+        // #1 preprocessing
+        Mat gray = preprocessingImg(img);
+        // #2 get contours of preprocessed img
+        List<MatOfPoint> contours = getContour(gray);
+        // #3 find biggest contour, sort maxarea corners
+        List<MatOfPoint2f> biggestContour = biggestContour(contours);
+        MatOfPoint2f sortedCorners = sortPoint(biggestContour);
+        // #4 get outline of img
+        MatOfPoint2f outline = getOutline(img);
+        //transoform img
+        Mat transformImg = Mat.zeros(img.size(), img.type());
+        Mat perspectiveTransform = Imgproc.getPerspectiveTransform(sortedCorners, outline);
+        Imgproc.warpPerspective(img, transformImg, perspectiveTransform, img.size());
+
+        return transformImg;
+    }
+
+    public void showImg(String name, Mat img) {
+        HighGui.imshow(name, img);
+        HighGui.waitKey();
+    }
+
+
+    public List<MatOfPoint> getContour(Mat img) {
+        //find contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(img, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+        /*
+        Drawing contours to display effect of preprocessing
+
+        Mat drawingContours = Mat.zeros(gray.size(), CvType.CV_8U);
+        Imgproc.drawContours(drawingContours, contours, -1, new Scalar(255), 2); */
+
+        return contours;
+    }
+
+    public Mat preprocessingImg(Mat img) {
+        resizeImg(img);
         Mat gray = new Mat();
-        Imgproc.cvtColor(src, gray, Imgproc.COLOR_RGB2GRAY);
-
-        Mat sobelx = new Mat();
-        Imgproc.Sobel(gray,sobelx, CvType.CV_32F,1,0,-1);
-        Mat sobely = new Mat();
-        Imgproc.Sobel(gray,sobely, CvType.CV_32F,0,1,-1);
-        Mat gradient = new Mat();
-        Core.subtract(sobelx,sobely,gradient);
-        Core.convertScaleAbs(gradient,gradient);
-        Mat blur = new Mat();
-        Imgproc.GaussianBlur(gradient,blur, new Size(9, 9), 0);
-        Mat threshold = new Mat();
-        Imgproc.threshold(blur, threshold,125,255,Imgproc.THRESH_BINARY);
-        Mat closed = new Mat();
-        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2));
-        Imgproc.morphologyEx(threshold, closed, Imgproc.MORPH_CLOSE,kernel );
-
-        HighGui.imshow("xd",closed);
-
-        HighGui.waitKey();
-
+        //blur img
+        Imgproc.medianBlur(img, gray, 9);
+        //swap to gray scale
+        Imgproc.cvtColor(gray, gray, Imgproc.COLOR_RGB2GRAY);
+        //canny detector
+        Imgproc.Canny(gray, gray, 85, 150);
+        return gray;
     }
+
+    public MatOfPoint2f sortPoint(List<MatOfPoint2f> biggestContour) {
+        //find of the center "mass" of our contour
+        Moments moment = Imgproc.moments(biggestContour.get(0));
+        int x = (int) (moment.get_m10() / moment.get_m00());
+        int y = (int) (moment.get_m01() / moment.get_m00());
+        double[] data;
+        Point[] sortedPoints = new Point[4];
+        int cnt = 0;
+        for (int i = 0; i < biggestContour.get(0).rows(); i++) {
+            data = biggestContour.get(0).get(i, 0);
+            double dataX = data[0];
+            double dataY = data[1];
+            if (dataX < x && dataY < y) {
+                sortedPoints[0] = new Point(dataX, dataY);
+                cnt++;
+            } else if (dataX > x && dataY < y) {
+                sortedPoints[1] = new Point(dataX, dataY);
+                cnt++;
+            } else if (dataX < x && dataY > y) {
+                sortedPoints[2] = new Point(dataX, dataY);
+                cnt++;
+            } else if (dataX > x && dataY > y) {
+                sortedPoints[3] = new Point(dataX, dataY);
+                cnt++;
+            }
+        }
+        MatOfPoint2f corners = new MatOfPoint2f(
+                sortedPoints[0],
+                sortedPoints[1],
+                sortedPoints[2],
+                sortedPoints[3]);
+        return corners;
+    }
+
+    //this function detect only 16:9 or 4:3 resolution!!!
+    public Mat resizeImg(Mat img) {
+        int height = img.height();
+        int width = img.width();
+        float propotion;
+        boolean isHorizontal = true;
+        if (height > width) {
+            propotion = (float) height / width;
+            isHorizontal = false;
+        } else {
+            propotion = (float) width / height;
+        }
+
+        if (Math.abs(propotion - 1.33) < 0.01) {
+            if (isHorizontal) {
+                Imgproc.resize(img, img, new Size(1024, 768));
+            } else {
+                Imgproc.resize(img, img, new Size(768, 1024));
+            }
+
+        } else {
+            if (isHorizontal) {
+                Imgproc.resize(img, img, new Size(1280, 720));
+            } else {
+                Imgproc.resize(img, img, new Size(720, 1280));
+            }
+
+        }
+
+
+        return img;
+    }
+
+    //get lineframe of img
+    public MatOfPoint2f getOutline(Mat img) {
+        Point topLeft = new Point(0, 0);
+        Point topRight = new Point(img.cols(), 0);
+        Point bottomRight = new Point(img.cols(), img.rows());
+        Point bottomLeft = new Point(0, img.rows());
+
+        Point[] points = {topLeft, topRight, bottomLeft, bottomRight};
+        MatOfPoint2f result = new MatOfPoint2f();
+        result.fromArray(points);
+        return result;
+    }
+
+    //find biggest of contours
+    public List<MatOfPoint2f> biggestContour(List<MatOfPoint> contours) {
+        List<MatOfPoint2f> biggestArea = new ArrayList<>();
+        double maxArea = 0;
+
+        for (MatOfPoint contour : contours) {
+            //arcLenght calculates a contour perimeter or a curve length.
+            MatOfPoint2f c2f = new MatOfPoint2f(contour.toArray());
+            double peri = Imgproc.arcLength(c2f, true);
+            // approximates a contour shape to antoher with less num of vertivles
+            MatOfPoint2f approx = new MatOfPoint2f();
+            Imgproc.approxPolyDP(c2f, approx, 0.02 * peri, true);
+            Point[] vertex = approx.toArray();
+            if ((Imgproc.contourArea(contour) > maxArea) && (vertex.length == 4)) {
+                biggestArea.add(approx);
+                maxArea = Imgproc.contourArea(contour);
+            }
+        }
+        return biggestArea;
+    }
+
 
 }
